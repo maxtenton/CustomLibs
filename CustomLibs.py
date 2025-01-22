96594c1b25f8bb2848e10afca4b417381e5fb82a
maxtenton <maxtenton@gmail.com>
2025-01-22 08:42:43 +0200

    Minor optimizations

--- CustomLibs.py
+++ CustomLibs.py
@@ -1,19 +1,24 @@
 from collections import namedtuple
-from math import pi
+from math import pi, sqrt
 
 class SpatialMath:
+
+    global vec2
+    vec2 = namedtuple("vector2", ["x", "y"])
+    global vec3
+    vec3 = namedtuple("vector3", ["x", "y", "z"])
+    global rot
+    rot = namedtuple("rotation", ["x", "y", "z"])
+
     def vector2(x, y):
-        vec2 = namedtuple("vector2", ["x", "y"])
         vec2var = vec2(x, y)
         return vec2var
 
     def vector3(x, y, z):
-        vec3 = namedtuple("vector3", ["x", "y", "z"])
         vec3var = vec3(x, y, z)
         return vec3var
 
     def rotation(x = 0, y = 0, z = 0, bRad = False):
-        rot = namedtuple("rotation", ["x", "y", "z"])
         if(bRad == True):
             rotvar = rot(x*180/pi, y*180/pi, z*180/pi)
             return rotvar
